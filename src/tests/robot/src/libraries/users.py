d1a9c5aac9d309cb21fad08011f6ce0aab7cf1fc
AndreiOlegovich <info@urn.su>
2023-12-07 18:06:05 +0200

    Adds type check to spoil_user() and func to delete multiple credentials

--- src/tests/robot/src/libraries/users.py
+++ src/tests/robot/src/libraries/users.py
@@ -1,5 +1,9 @@
 import random
+from robot.api import logger
 import utils
+
+
+CREDENTIALS = ["username", "password", "firstname", "lastname", "phone"]
 
 
 def generate_phone():
@@ -19,7 +23,42 @@ def generate_user() -> dict:
 
     return user
 
-def spoil_user(user: dict, attr: str):
-    user[attr] = ""
+
+def select_credentials_to_delete():
+    creds = CREDENTIALS
+
+    # How many credentials to delete (min 2)
+    num = random.randint(2, len(creds) - 1)
+    creds_to_delete = []
+
+    # Index of first credential to be deleted
+    j = random.randint(0, len(creds) - 1)
+
+    i = 0
+    while i < num:
+        while creds[j] in creds_to_delete:
+            j = random.randint(0, len(creds) - 1)
+        creds_to_delete.append(creds[j])
+        logger.info(i)
+        i += 1
+        logger.info(i)
+
+    return creds_to_delete
+
+
+def spoil_user(user: dict, attrs):
+    if isinstance(attrs, str):
+        attrs = [attrs]
+    elif isinstance(attrs, list):
+        pass
+    else:
+        raise TypeError()
+
+    logger.info(attrs)
+    filter = CREDENTIALS
+    attrs_to_remove = [k for k in attrs if k in filter]
+    logger.info(attrs_to_remove)
+    for attr in attrs_to_remove:
+        user[attr] = ""
+
     return user
-    
